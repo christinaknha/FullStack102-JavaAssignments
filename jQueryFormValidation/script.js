54ab554224758585426dbd03aca99fcfd2d1b061
Christina Ha <christinaknha@gmail.com>
2023-04-25 16:58:53 -0400

    first version of jQueryForm

--- jQueryFormValidation/script.js
+++ jQueryFormValidation/script.js
@@ -1,9 +1,9 @@
 // submit button clicked will fire
-$(`#submit`).on(`click`, $(document).ready(function(){
+$(document).ready($('#submit').on('click', () => {
     // Create variables for each of the form inputs
-    let name = $(`#name`);
-    let email = $(`#email`);
-    let phoneNumber = $(`#phone`);
+    let name = $('#name');
+    let email = $('#email');
+    let phoneNumber = $('#phone');
 
     // Create an array to store variables
     let required = [name, email, phoneNumber]
@@ -11,20 +11,30 @@ $(`#submit`).on(`click`, $(document).ready(function(){
     // Creates a for loop to iterate through the array
     for( let i = 0; i < required.length; i++){
         // check to see if the value is empty
-        if (required[i].val == ""){
+        if (required[i].val() === ""){
             // print message 
-            $(`message`).text = "Please Fill Out Required Fields"
+            $('#message').text("Please Fill Out Required Fields")
             // add warning class to this message
-            $(`message`).addClass("warning");
+            $('#message').addClass("warning");
             // add warning class to array's items
-            $('label').addClass("warning");
+            $('label').eq(i).addClass("warning");
+            
+            $('input').eq(i).on("change", function(){
+                $('label').eq(i).removeClass("warning")
+            })
+        //     $('label').toggleClass(function(){
+        //     if (required[i].val() != "") {
+        //         $('label').eq(i).removeClass("warning")
+        //     } else {
+        //         $('label').eq(i).addClass("warning");
+        //     })
         } else {
             $('label').removeClass("warning");
         }
     }
-
-    if ($('label').hasClass("warning")){
-        
+    if (!$('label').hasClass("warning")){
+        $('form').remove();
+        $('h2').text("Thanks for your feedback!") 
     }
 })
 );
