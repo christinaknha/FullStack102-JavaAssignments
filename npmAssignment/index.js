8bd5a84ff2f5d227c1ce563264ddead18b13c9d3
Christina Ha <christinaknha@gmail.com>
2023-04-25 10:55:11 -0400

    first commit of npm assignment

---
+++ npmAssignment/index.js
@@ -0,0 +1,11 @@
+console.log("my application just ran")
+
+const Moment = require('moment')
+const a = new Moment();
+console.log(a.format('h:mm:ss a') + " is when my application ran")
+
+// import Moment from 'moment';
+
+// const a = new Moment();
+// console.log(a.format('h:mm:ss a') + " is when my application ran")
+
